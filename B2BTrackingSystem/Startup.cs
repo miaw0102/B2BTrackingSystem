17dcdb70ec94aa8d70326a9931bc568359b4fa42
buttoncywang <cywang0404@g2.nctu.edu.tw>
2017-07-18 16:19:31 +0800

    加入專案檔案。

---
+++ B2BTrackingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(B2BTrackingSystem.Startup))]
+namespace B2BTrackingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
